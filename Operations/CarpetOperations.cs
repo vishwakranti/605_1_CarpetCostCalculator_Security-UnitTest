170750883aefaa7220aeea0a9299027b55d340b3
Vishwakranti Suryawanshi/Musalgavkar <87359394+vishwakranti@users.noreply.github.com>
2023-03-13 19:03:18 +1300

    Add project files.

---
+++ Operations/CarpetOperations.cs
@@ -0,0 +1,27 @@
+﻿namespace CarpetCostCalculator.Operations
+{
+    public static class CarpetOperations
+    {
+
+        public static decimal CalculateRoomArea(decimal length, decimal width)
+        {
+            return length * width;
+        }
+
+        public static decimal CalculateCarpetFinalCost(int carpetTypeCost, decimal areaOfCarpet, 
+                                                        bool installation, bool underlay,       
+                                  
+                                                        decimal installationCost, decimal underlayCost,
+                                                        decimal costPerSquareMeter)
+        {
+            decimal finalCost = carpetTypeCost * areaOfCarpet *costPerSquareMeter;
+            if (installation)
+                finalCost = finalCost + installationCost;
+
+            if (underlay)
+                finalCost = finalCost + underlayCost;
+            return finalCost;
+        }
+
+    }
+}
